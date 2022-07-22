7868c8b2ed69c4e6434762a28c60cf6a49582906
Yeisson <yeisson.meza.27@unsch.edu.pe>
2022-07-21 22:56:39 -0500

    Merge branch 'master' of https://github.com/Inmortal175/Kallpa_Skins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './Admin-Panel/main/main.component';
 import { DashBoardComponent } from './Admin-Panel/dash-board/dash-board.component';
+import { AddProductComponent } from './componentes/add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    DashBoardComponent
+    DashBoardComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
