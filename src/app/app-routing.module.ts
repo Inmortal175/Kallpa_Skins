8a0fbf074c71cbbbce017867a00161c749cad7bc
Jesus <jesus.vilca.27@unsch.edu.pe>
2022-07-22 19:30:04 -0500

    ACTUALIZACION
    registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Admin-Panel/login/login.component';
 import { MainComponent } from './Admin-Panel/main/main.component';
-import { PassComponent } from './Admin-Panel/pass/pass.component';
 import { RegistroComponent } from './Admin-Panel/registro/registro.component';
 
 const routes: Routes = [
   {path: 'main',  component: MainComponent },
   {path: 'login', component:  LoginComponent },
-  {path: `pass`,  component:  PassComponent },
+ 
   {path: `registro`,  component:  RegistroComponent},
 ];
 
